af31a9ddc7553017bf531c7f159cb5cb6cf5472e
Sammy275 <samfan95@gmail.com>
2023-06-19 00:17:43 +0500

    Added closing button in drawer

--- lib/common/widgets/main_drawer.dart
+++ lib/common/widgets/main_drawer.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+
 import 'package:accounting_app/features/journal/screens/main_journal_screen.dart';
-import 'package:accounting_app/features/reporting/screens/main_report_options_screen.dart';
 import 'package:accounting_app/features/t_account/screens/display_account_main_screen.dart';
+import 'package:accounting_app/features/reporting/screens/main_report_options_screen.dart';
+
+import 'package:accounting_app/helpers/network_request_maker.dart';
 
 
 
@@ -37,27 +42,42 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          _generateDrawerTile(
-            context,
-            Icons.book_outlined,
-            'Journal',
-            MainJournalScreen.routeName,
-          ),
-          _generateDrawerTile(
-            context,
-            Icons.account_balance,
-            'Accounts',
-            DisplayAccountMainScreen.routeName,
-          ),
-          _generateDrawerTile(
-            context,
-            Icons.report,
-            'Reports',
-            MainReportOptionsScreen.routeName,
-          ),
-        ],
+      child: SafeArea(
+        child: Column(
+          children: [
+            _generateDrawerTile(
+              context,
+              Icons.book_outlined,
+              'Journal',
+              MainJournalScreen.routeName,
+            ),
+            _generateDrawerTile(
+              context,
+              Icons.account_balance,
+              'Accounts',
+              DisplayAccountMainScreen.routeName,
+            ),
+            _generateDrawerTile(
+              context,
+              Icons.report,
+              'Reports',
+              MainReportOptionsScreen.routeName,
+            ),
+            const Expanded(child: SizedBox.shrink()),
+            ListTile(
+              leading: const Icon(Icons.close),
+              title: const Text('Perform Closing'),
+              textColor: Colors.red,
+              iconColor: Colors.red,
+              onTap: () {
+                final postJsonData = json.encode({});
+
+                NetworkRequestMaker.postJsonDataInRequest('closing/close-journal-entries', postJsonData);
+                Navigator.of(context).pushReplacementNamed(MainJournalScreen.routeName);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
